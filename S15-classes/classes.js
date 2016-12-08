3484f159cef0db8da7796d8cff7d739d1c8678ae
Will Batey <will@willbatey.co.uk>
2016-12-08 13:01:47 +0000

    Progressed on ES6 classes

--- S15-classes/classes.js
+++ S15-classes/classes.js
@@ -8,50 +8,24 @@
 // JavaScript classes provide a much simpler and clearer syntax to create objects and deal with inheritance.
 
 
-(function(global) {
-
-// We use the typical example of a 'Car' object and then use different brands to inherit different properties.
-
-	const Car = function(options) {
-	
-		// Borrowing from jQuery and the Greetr project, we return a 
-		// new Car.init object to avoid using 'new' all the time.
-
-		return new Car.init(options);
-	};
+// We are going to use the example of a car object and then create another objetc that inherits from it.
 
 
-	// The car prototype is where our methods and properties will live.
+(function() {
+
+	function Car(options) {
+		this.title = options.title;
+	}
 
 	Car.prototype = {
-
 		drive() {
 			console.log('vroom');
 		}
-
 	};
 
+	const car = new Car({ title: 'Focus' });
 
-	// Our init function creates a new car object.
+	console.log(car);
+	car.drive();
 
-	const init = Car.init = function(options) {
-		this.title = options.title;
-	};
-
-
-	// Set the prototype of objects created from our Car.init function constructor to 
-	// the prototype of Car.prototype.
-
-	Car.init.prototype = Car.prototype;
-
-	// Expose Car object to the global object for use as window.Car or window.C$
-
-	global.Car = global.C$ = Car;
-
-
-// Set global to window
-
-})(window);
-
-const carA = C$({title:"Focus"});
-console.log(carA);
+})();
