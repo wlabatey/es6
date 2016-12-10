a8efb6e6acb9d4679a38047a4aa1392f3d3d9e61
Will Batey <will@willbatey.co.uk>
2016-12-10 08:44:27 +0000

    Progressed on generators... wow.

--- S16-generators/generators.js
+++ S16-generators/generators.js
@@ -6,19 +6,24 @@
 // Set, String, TypedArray, arguments object and so on), invoking a custom iteration hook with statements 
 // to be executed for the value of each distinct property.
 
-const colors = ['red', 'green', 'blue'];
+(function(){
 
-for (let color of colors) {
-	console.log(color);
-}
+	const colors = ['red', 'green', 'blue'];
 
-const numbers = [1,2,3,4];
+	for (let color of colors) {
+		console.log(color);
+	}
 
-let total = 0;
-for (let number of numbers) {
-	total += number;
-	console.log(total);
-}
+	const numbers = [1,2,3,4];
+
+	let total = 0;
+	for (let number of numbers) {
+		total += number;
+		console.log(total);
+	}
+
+})();
+
 
 // Generators let us run some code, return a value and then return to the function and continue executing code.
 
@@ -33,19 +38,204 @@ for (let number of numbers) {
 // whether the generator has yielded its last value as a boolean. Calling the next() method with an argument will resume 
 // the generator function execution, replacing the yield statement where execution was paused with the argument from next(). 
 
-function* shopping() {
+(function() {
+
+	function* shopping() {
+		
+		// stuff on the sidewalk
+
+		// walking down the sidewalk
+
+		// go int othe store with cash
+
+		const stuffFromStore = yield 'cash';	
+
+		// walking to laundry place
+
+		const cleanClothes = yield 'laundry';
+
+		// walking back home
+
+		return [stuffFromStore, cleanClothes];
+	}
+
+	// Stuff happening in the store
+
+	const gen = shopping(); // Invoking our generator function with parens doesn't actually do anything.
+
+	console.log(gen); // shopping generator object
+
+	console.log(gen.next()); // leaving our house
+
+	// walk into the store
+	// walk up and down the aisles
+	// purchase our stuff
+
+	console.log(gen.next('groceries')); // leaving the store with groceries
+
+	console.log(gen.next('clean clothes')); // leave the laundry place with clean clothes
+ 
+ })();
+
+// Another example with a colors generator function.
+
+
+(function(){
+
+	function* colors() {
+		yield 'red';
+		yield 'blue';
+		yield 'green';
+	}
+
+	// const gen = colors();
+	// console.log(gen.next());
+	// console.log(gen.next());
+	// console.log(gen.next());
+
+	// Stephen's "big reveal": generators work perfectly with for...of loops. Below is the example.
+
+	// Our loop iterates through our generator and pushes each result into our myColors array.
+
+	const myColors = [];
+
+	for (let color of colors()) {
+		myColors.push(color);
+	}
+	console.log(myColors); // ["red", "blue", "green"]
+
+	// Generators can be used to iterate over any data structure we want, which is one of their biggest uses.
+
+})();
+
+// Below is another example, showing where generator functions can be useful.
+
+// We are first creating an object that represents a software engineering team.
+
+// We then create another object representing a testing team, which will be separate from the engineering team.
+
+// We then combine generator functions.
+
+
+(function(){
+
+	const testingTeam = {
+		lead: 'Amanada',
+		tester: 'Bill'
+	};
+
+	const engineeringTeam = {
+
+		testingTeam,
+		size: 3,
+		department: 'Engineering',
+		lead: 'Jill',
+		manager: 'Alex',
+		engineer: 'Dave'
+
+	};
+
+	// What we might use a generator for is to create a function that only iterates through our employee keys on the above object,
+	// not the size or department.
+
+	function* TeamIterator(team) {
+		yield team.lead;
+		yield team.manager;
+		yield team.engineer;
+		const testTeamGenerator = TestTeamIterator(team.testingTeam);
+
+		// The yield* keyword can be thought of as a trap door that lets our for..of loop fall into our TestTeamIterator generator
+		// and lets it also iterate over our other yield statements in the TeamTeamIterator generator.
+
+		// This is called Generator Delegation.
+
+		yield* testTeamGenerator;
+	}
+
+	function* TestTeamIterator(team) {
+		yield team.lead;
+		yield team.tester;
+	}
+
+	const myTeam = [];
+
+	for (let member of TeamIterator(engineeringTeam)) {
+		myTeam.push(member);
+	}
+	console.log(myTeam);
+
+	console.log("-----------------------------------------------------------------");
+
+})();
+
+// Next, we take the above code and refactor it to make it more concise.
+
+// We use symbol iterators to do this.
+
+// The way it helps is that it allows us to merge our Iterator generators into our two team objects.
+
+// Symbol iterator is a special object in ES6 that teaches objects how to deal with for...of loop.
+
+
+
+(function(){
+
+	const testingTeam = {
+		lead: 'Amanada',
+		tester: 'Bill',
+
+		// Our for...of loop discovers the key called [Symbol.iterator] and if it exists, 
+		// it will use the generator function it is pointing at for iteration.
+
+		// In ES6 we can use key interpolation by using square brackets around the key name.
+		// This does not create an array, but allows us to have a dynamically generated key.
+
+		[Symbol.iterator]: function* () {
+
+			// We use 'this' here to refer to the testingteam object.
+
+			yield this.lead;
+			yield this.tester;
+		}
+	};
+
+	const engineeringTeam = {
+
+		testingTeam,
+		size: 3,
+		department: 'Engineering',
+		lead: 'Jill',
+		manager: 'Alex',
+		engineer: 'Dave',
+		[Symbol.iterator]: function* () {
+
+			yield this.lead;
+			yield this.manager;
+			yield this.engineer;
+
+			// We use yield* here to make sure that when iterating through our engineeringTeam object,
+			// we also drop down into our testingTeam object and also walk through that too.
+
+			yield* testingTeam;
+		}
+
+	};
+
+	const myTeam = [];
+
+	// We no longer need to use a separately defined generator function.
+
+	// We can just pass our engineeringTeam object and our for...of loop will find the [Symbol.iterator] object
+	// which will use the generator to iterate through the object and also the testingTeam object to produce
+	// the same result as before.
+
+	for (let member of engineeringTeam) {
+		myTeam.push(member);
+	}
+
+	console.log(myTeam);
+
+	console.log("-----------------------------------------------------------------");
+
+})();
 	
-	// stuff on the sidewalk
-
-	// walking down the sidewalk
-
-	// go int othe store with cash
-	yield 'cash';
-}
-
-// Stuff happening in the store
-
-const gen = numbers2();
-console.log(gen.next()); // { "done": false }
-console.log(gen.next()); // { "done": true }
-
